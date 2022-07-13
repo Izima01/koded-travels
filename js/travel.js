f2e9bd7a11dd37aa6bb491afb25354c57d496337
OfficialOkanlawon <okanlawonimmanuel@gmail.com>
2022-07-13 15:45:54 +0100

    ch: Travel guide profile page

--- js/travel.js
+++ js/travel.js
@@ -12,3 +12,22 @@ questions.forEach(function(question){
     })
 
 })
+
+window.addEventListener('scroll', show);
+
+function show(){
+    let shows = document.querySelectorAll('.show');
+
+    for(let i = 0; i < shows.length; i++){
+
+        let windowheight = window.innerHeight;
+        let showtop = shows[i].getBoundingClientRect().top;
+        let showpoint = 150;
+
+        if(showtop < windowheight - showpoint){
+            shows[i].classList.add('active');
+        }else{
+            shows[i].classList.remove('active');
+        }
+    }
+}
